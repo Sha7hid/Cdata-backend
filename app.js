ceaa40cbf89e9efc3ba8b9fa59fe1eee458c5959
sha7hid <shahidshafadshahad@gmail.com>
2023-02-22 15:13:06 +0530

    edited

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 require('dotenv').config();
 
-const { createAgent } = require('@forestadmin/agent');
-const { createMongooseDataSource } = require('@forestadmin/datasource-mongoose');
-// Retrieve your mongoose connection
-const connection = require('./config/db');
-
-// Create your Forest Admin agent
-// This must be called BEFORE all other middleware on the app
-createAgent({
-  authSecret: process.env.FOREST_AUTH_SECRET,
-  envSecret: process.env.FOREST_ENV_SECRET,
-  isProduction: process.env.NODE_ENV === 'production',
-  
-})
-  // Create your Mongoose datasource
-  .addDataSource(createMongooseDataSource(connection))
-  // Replace "myExpressApp" by your Express application
-  .mount(application)
-  .start();
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -27,7 +9,6 @@ const app = express();
 app.use(bodyParser.json());
 
 const connectDB = require('./config/db');
-const { application } = require('express');
 
 const mongo_uri =`mongodb+srv://shahid:arthur540913@cluster2.ggcnvuy.mongodb.net/cdata`
 
